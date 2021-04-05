b434d6c9f98aeec23261285b3c839b201b2712fa
NEO <neo.one.mail@gmail.com>
2021-04-05 19:48:33 +0200

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { config } from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import fs from 'fs';
+import path from 'path';
+
+config();
+
+(async () => {
+    const app = express();
+    const port = process.env.PORT || 8080;
+
+    app.use(helmet());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+
+    if (process.env.NODE_ENV === 'production') {
+        const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+        app.use(morgan('combined', { stream: accessLogStream }));
+    } else {
+        app.use(morgan('dev'));
+    }
+
+    app.get('/', (_, res) => res.json({ msg: 'Hello World!' }));
+
+    app.listen(port, () => console.log(`Server is running on ${port} ...`));
+})();
